993d9d910bebb5785874cd7326f2f708384bbd0d
seominjeong <smj12938@gmail.com>
2022-06-04 11:40:40 +0900

    - Todo 작업중

--- react-app/src/App.js
+++ react-app/src/App.js
@@ -5,44 +5,29 @@ import "./App.css";
 import "./style.css";
 
 function App() {
-  const [showing, setShowing] = useState(false);
-  const [counter, setValue] = useState(0);
-  const [keyword, setKeyword] = useState("");
-  // const onClick = () => setValue((prev) => prev + 1);
-  const onChange = (event) => setKeyword(event.target.value);
-  const onClick = () => setShowing((prev) => !prev);
-
-  // useEffect는 코드가 딱 한번만 실행될 수 있도록 도와줌.
-  useEffect(() => {
-    console.log("Call the Api...");
-  }, []);
-
-  useEffect(() => {
-    if (keyword !== "" && keyword.length > 5) {
-      console.log("Seach For", keyword);
+  const [toDo, setToDo] = useState("");
+  const [toDos, setToDos] = useState([]);
+  const onChange = (event) => setToDo(event.target.value);
+  const onSubmit = (event) => {
+    event.preventDefault();
+    if (toDo === "") {
+      return;
     }
-    // 하나라도 변할 시 실행되라 하는 코드.
-  }, [keyword, counter]);
-
-  function Hello() {
-    useEffect(() => {
-      console.log("create:)");
-      return () => console.log("booya:(");
-    }, []);
-  }
-
+    setToDos((currentArray) => [toDo, ...currentArray]);
+    setToDo("");
+  };
   return (
     <div>
-      {/* <input
-        value={keyword}
-        onChange={onChange}
-        type="text"
-        placeholder="Search here..."
-      />
-      <h1>{counter}</h1>
-      <button type="button" onClick={onClick}></button> */}
-      {showing ? <Hello /> : null}
-      <button onClick={onClick}>{showing ? "hide" : "show"}</button>
+      <h1>My To Dos ({toDos.length})</h1>
+      <form onSubmit={onSubmit}>
+        <input
+          onChange={onChange}
+          value={toDo}
+          type="text"
+          placeholder="Write your to do..."
+        />
+        <button>Add To Do</button>
+      </form>
     </div>
   );
 }
